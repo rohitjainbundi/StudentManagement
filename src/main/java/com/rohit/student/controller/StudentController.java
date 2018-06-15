5955ba9e934ea5d8d77e5df9cd0b393df1052342
rohjain0 <rohjain0@WKWIN5620342.global.publicisgroupe.net>
2018-06-15 10:20:47 +0530

    added to master

--- src/main/java/com/rohit/student/controller/StudentController.java
+++ src/main/java/com/rohit/student/controller/StudentController.java
@@ -5,4 +5,12 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class StudentController {
 
+	public static void main(String[] args) {
+		
+	int i = 5;
+	int j= 5;
+	if(i==j) {
+		System.out.println("Matched");
+	}
+	}
 }
